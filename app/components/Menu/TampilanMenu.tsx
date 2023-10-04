73855228958731a8fb85dbd415781ccdc5d612d1
Bryan-snw <Judithbryan_19@kharisma.ac.id>
2023-10-04 17:48:07 +0800

    Done tampilanMenu

--- app/components/Menu/TampilanMenu.tsx
+++ app/components/Menu/TampilanMenu.tsx
@@ -1,8 +1,115 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+const rupiah = (number: number) =>{
+    return new Intl.NumberFormat("id-ID", {
+      style: "currency",
+      currency: "IDR"
+    }).format(number);
+  }
 
 const TampilanMenu = () => {
+
+    const [namaMakanan,setNamaMakanan] = useState("");
+    const [hargaMakanan,setHargaMakanan] = useState(0);
+
+    const [menuList, setMenuList] = useState([
+        {
+            id: 1,
+            nama: "Nasi Goreng",
+            harga: 20000
+        },
+        {
+            id: 2,
+            nama: "Mie Goreng",
+            harga: 22000
+        },
+        {
+            id: 3,
+            nama: "Ayam Goreng",
+            harga: 15000
+        },
+    ]);
+
+    function handleDelete() {
+        
+    }
+    
+
+    function handleAddMenu(e: any){
+        e.preventDefault();
+
+        if (namaMakanan === "") {
+            alert("Data Masih Kosong");
+        } else {
+
+            const updatedData = [...menuList];
+
+            const data = {
+                id: menuList.length+1,
+                nama: namaMakanan,
+                harga: hargaMakanan
+            }
+
+            updatedData.push(data);
+
+            setMenuList(updatedData);
+            setHargaMakanan(0);
+            setNamaMakanan("")
+        }
+
+    }
+
   return (
-    <div>TampilanMenu</div>
+    <>
+        <table className="w-full">
+            <thead>
+            <tr>
+                    <th className="text-start">ID</th>
+                    <th className="text-start">Nama</th>
+                    <th className="text-start">Harga</th>
+                    <th className="text-start">Aksi</th>
+                </tr>
+            </thead>
+            <tbody>
+                {menuList.map(menu => 
+                    <tr key={menu.id}>
+                        <td>{menu.id}</td>
+                        <td>{menu.nama}</td>
+                        <td>{rupiah(menu.harga)}</td>
+                        <td><span className="cursor-pointer"onClick={handleDelete}>Hapus</span></td>
+                    </tr>
+                )}
+            </tbody>
+        </table>
+        <hr className='my-4 h-px bg-gray-200 border-2 rounded-xl' />
+        <form className="grid gap-3" onSubmit={handleAddMenu}>
+            <div>
+                <label htmlFor="namaMakanan" className="font-semibold">Nama Makanan</label>
+                <input className="w-full rounded-md p-2 mt-1" 
+                    type="text" 
+                    placeholder="Nama Makanan"
+                    id='namaMakanan'
+                    name='namaMakanan'
+                    value={namaMakanan}
+                    required
+                    onChange={(e)=>setNamaMakanan(e.target.value)}
+                />
+            </div>
+            <div>
+                <label htmlFor="hargaMakanan" className="font-semibold">Harga Makanan</label>
+                <input className="w-full rounded-md p-2 mt-1" 
+                    type="number" 
+                    placeholder="10000"
+                    id='hargaMakanan'
+                    name='hargaMakanan'
+                    value={hargaMakanan}
+                    onChange={(e)=>setHargaMakanan(parseInt(e.target.value))}
+                />
+            </div>
+            <button type='submit' className="mt-1 w-full bg-green-500 hover:bg-green-600 py-2 text-white font-semibold rounded-xl">Tambah Menu</button>
+        </form>
+    </>
   )
 }
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,45 @@
 'use client';
 import Image from 'next/image'
+import TampilanMenu from './components/Menu/TampilanMenu';
+import TampilanMeja from './components/Meja/TampilanMeja';
+import TampilanNota from './components/Nota/TampilanNota';
+import TampilanPesanan from './components/Pesanan/TampilanPesanan';
+import { useState } from 'react';
 
 
 export default function Home() {
 
-  function handleClick(type: string){
-    alert(type);
+  const [type, setType] = useState('menu');
+
+  function renderView(){
+    switch (type) {
+      case 'menu':
+        return <TampilanMenu />;
+      case 'meja':
+        return <TampilanMeja />
+      case 'pesanan':
+        return <TampilanPesanan />
+      case 'nota':
+        return <TampilanNota />
+    }
+  }
+
+  function changeView(type: string){
+    
+    setType(type);
   }
 
   return (
     <main className="flex min-h-screen items-center justify-center">
-      <div className="grid bg-slate-400 p-20  w-auto">
-        <div>
-          <button onClick={()=>handleClick('menu')} className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Menu</button>
-          <button className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Meja</button>
-          <button className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Pesanan Aktif</button>
-          <button className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Cetak Nota</button>
+      <div className="grid bg-slate-400 p-5 rounded-lg">
+        <div className="flex gap-3 mb-3">
+          <button onClick={()=>changeView('menu')} className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Menu</button>
+          <button onClick={()=>changeView('meja')} className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Meja</button>
+          <button onClick={()=>changeView('pesanan')} className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Pesanan Aktif</button>
+          <button onClick={()=>changeView('nota')} className='p-3 bg-blue-500 hover:bg-blue-700 text-white rounded-lg'>Cetak Nota</button>
         </div>
         <div>
-          
+          {renderView()}
         </div>
       </div>
     </main>
